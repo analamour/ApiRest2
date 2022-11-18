05bd0df6e95b0ac8ebc54d08a2dbfbe23eeafebe
Anabella Lamour <ana.lamour@outlook.com>
2022-11-18 19:49:06 -0300

    index

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require ('express'); 
+const app = express ();
+const morgan = require ('morgan');
+
+
+//settings
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2)
+
+
+//middleares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+//routes
+app.get('/', (req,res) => {
+    res.json({"Titulo": "Hello World"});
+
+});
+
+
+
+
+// start server
+app.listen (app.get('port'), () => {
+    console.log(`server on port ${app.get('port')}`);
+});
